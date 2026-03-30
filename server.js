a5846bc2d0e76089205e563bda577c217ec58cad
vyshalisathyaseelan <vanivyshali@gmail.com>
2026-03-30 09:34:34 +0530

    backend initial commit

--- server.js
+++ server.js
@@ -27,6 +27,8 @@ app.post("/login", (req, res) => {
 })
 
 
-app.listen(5000, () => {
-  console.log("Server is running...")
-})
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
+});
